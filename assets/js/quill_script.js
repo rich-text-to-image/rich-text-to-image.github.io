663f3209d5109bba8eadb0ba5ad7f64a802cc760
Songwei Ge <songweige@songweis-air.wifi.local.cmu.edu>
2023-03-28 14:47:45 -0400

    footnote, links

--- assets/js/quill_script.js
+++ assets/js/quill_script.js
@@ -1,7 +1,7 @@
 $("#text2json").click(function () { text2json() });
 
 function text2json() {
-    var jsonString = JSON.stringify(quill.getContents(), null, 2)
+    var jsonString = JSON.stringify(quill.getContents())
     var jsonContainer = document.getElementById("json-container");
     jsonContainer.textContent = jsonString;
 };
